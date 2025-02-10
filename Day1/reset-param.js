34696a5d56c5f6bf4aff530e6c836886d3f2a246
unknown <dthann2005@gmail.com>
2025-02-10 11:46:16 +0700

    Day01

---
+++ Day1/reset-param.js
@@ -0,0 +1,10 @@
+
+function fn_reset_param(a,b,...other){
+    console.log("a=",a);
+    console.log("b=",b);
+    console.log("...other",other);
+}
+
+fn_reset_param(1,2);
+fn_reset_param(1,2,4,5);
+fn_reset_param(1,2,4,5,100,200);
