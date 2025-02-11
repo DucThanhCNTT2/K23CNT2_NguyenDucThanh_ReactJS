c4227b8e7cef16500944d3ad3b8155b8a5ebda65
unknown <dthann2005@gmail.com>
2025-02-11 20:22:18 +0700

    Day02

---
+++ Day2/ndt-lab02/src/App.js
@@ -0,0 +1,22 @@
+function MyPage() {
+  return (
+    <a href="https://www.facebook.com/ucthanh.364915" target="_blank" rel="noopener noreferrer">
+      <button>
+        My personal Socialmedia
+      </button>
+    </a>
+  );
+}
+
+export default function Introduce() {
+  return (
+    <div>
+      <h1>Họ và tên: Nguyễn Đức Thành</h1>
+      <h2>Mã sinh viên: 2310900098</h2>
+      <h2>Ngày sinh: 03/04/2005</h2>
+      <h2>Điện thoại: 0367907165</h2>
+      <h2>Tên lớp: K23CNT2</h2>
+      <MyPage />
+    </div>
+  );
+}
